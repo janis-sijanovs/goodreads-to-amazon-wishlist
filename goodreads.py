3544df0c7fa062d4c5dd897f90c03e305a7ea7f3
janis-sijanovs <janis.sijanovs@gmail.com>
2023-06-29 19:42:22 +0100

    Fixed Infinite list size

--- goodreads.py
+++ goodreads.py
@@ -8,17 +8,17 @@ from helpers import clean_string
 def get_goodreads_book_list(list_ID: str) -> List[str]:
     """Returns a cleaned list of book titles from goodreads.com"""
     goodreads_session = requests.Session()
+    book_titles = []
     end_of_list = False
     page = 1
 
     while not end_of_list:
-        goodreads_to_read_url = f"https://www.goodreads.com/review/list/{list_ID}?shelf=to-read?page={page}"
+        goodreads_to_read_url = f"https://www.goodreads.com/review/list/{list_ID}?page={page}&shelf=to-read"
         goodreads_response = goodreads_session.get(goodreads_to_read_url)
         goodreads_soup = BeautifulSoup(goodreads_response.text, "html.parser")
         page += 1
 
         # Extract book titles from Goodreads "To-Read" list
-        book_titles = []
         book_elements = goodreads_soup.select(".title div.value a")
         for element in book_elements:
             book_titles.append(clean_string(element.text.strip()))
