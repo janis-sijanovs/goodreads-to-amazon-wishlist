94bd97322daa23afaa705b47e419f6252d54152c
janis-sijanovs <janis.sijanovs@gmail.com>
2023-06-29 19:30:22 +0100

    Added infinite pages

--- goodreads.py
+++ goodreads.py
@@ -8,14 +8,21 @@ from helpers import clean_string
 def get_goodreads_book_list(list_ID: str) -> List[str]:
     """Returns a cleaned list of book titles from goodreads.com"""
     goodreads_session = requests.Session()
-    goodreads_to_read_url = f"https://www.goodreads.com/review/list/{list_ID}?shelf=to-read"
-    goodreads_response = goodreads_session.get(goodreads_to_read_url)
-    goodreads_soup = BeautifulSoup(goodreads_response.text, "html.parser")
+    end_of_list = False
+    page = 1
 
-    # Extract book titles from Goodreads "To-Read" list
-    book_titles = []
-    book_elements = goodreads_soup.select(".title div.value a")
-    for element in book_elements:
-        book_titles.append(clean_string(element.text.strip()))
+    while not end_of_list:
+        goodreads_to_read_url = f"https://www.goodreads.com/review/list/{list_ID}?shelf=to-read?page={page}"
+        goodreads_response = goodreads_session.get(goodreads_to_read_url)
+        goodreads_soup = BeautifulSoup(goodreads_response.text, "html.parser")
+        page += 1
+
+        # Extract book titles from Goodreads "To-Read" list
+        book_titles = []
+        book_elements = goodreads_soup.select(".title div.value a")
+        for element in book_elements:
+            book_titles.append(clean_string(element.text.strip()))
+        if not book_elements:
+            end_of_list = True
 
     return book_titles
